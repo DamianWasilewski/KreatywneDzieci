d2c914e4b94c228b1c59213a2b36471df27962c5
unknown <damian.wasilewski1993@gmail.com>
2019-02-15 19:43:27 +0100

    Updates

---
+++ precache-manifest.89a2cd60f45ef0784748aa41f9d21589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c070b7bbdbaeccb4f5a329110d7cb8b2",
+    "url": "/KreatywneDzieci/static/media/tlo.c070b7bb.jpg"
+  },
+  {
+    "revision": "4cc8195a9bcc0d684345",
+    "url": "/KreatywneDzieci/static/js/runtime~main.4cc8195a.js"
+  },
+  {
+    "revision": "972d1a51a93d8e1d9a7d",
+    "url": "/KreatywneDzieci/static/js/main.972d1a51.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/KreatywneDzieci/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "972d1a51a93d8e1d9a7d",
+    "url": "/KreatywneDzieci/static/css/main.198c08d1.chunk.css"
+  },
+  {
+    "revision": "ff1d161bd924049db7f9cda24b423405",
+    "url": "/KreatywneDzieci/index.html"
+  }
+];
