05197909577fe1ae718f489dca13124d91284c30
unknown <damian.wasilewski1993@gmail.com>
2019-02-18 19:49:52 +0100

    Visual and RWD changes to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,7 @@ const home = () => (
   <div className="home">
     <div className="home__welcome">
       <span className="home__welcome_text">Kreatywne dzieci</span>
-      <span className="home__under-welcome_text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis porttitor orci quis maximus. Maecenas semper, nisi quis ultrices dictum, erat lorem tempor orci, non."</span>
+      <span className="home__under-welcome_text">„Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis porttitor orci quis maximus. Maecenas semper, nisi quis ultrices dictum, erat lorem tempor orci, non.”</span>
     </div>
   </div>
 );
